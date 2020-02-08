7734325b2860d1d86d4e49fcb461af1cbd40fd68
Yura0427 <yura.04.27@gmail.com>
2020-02-08 16:33:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ITask } from './task.interface';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  name: string;
+  process: boolean;
+  arrTask: Array<ITask> = [];
+  valueStatus: boolean;
+  index: number;
+  editName: string;
+  editStatus: boolean;
+  taskID: number;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  addTask() {
+    if (!this.name) {
+      this.valueStatus = true;
+    }
+    else {
+      const newT: ITask = new Task(1, this.name, false);
+      if (this.arrTask.length > 0) {
+        newT.id = this.arrTask.slice(-1)[0].id + 1;
+      }
+      this.arrTask.push(newT);
+      this.name = '';
+      this.valueStatus = false;
+    }
+  }
+  chec(event, i): void {
+    event.target.checked ? this.arrTask[i].process = true : this.arrTask[i].process = false;
+  }
+  deleteTask(i) {
+    this.arrTask.splice(i, 1);
+    for (let i = 0; i < this.arrTask.length; i++) this.arrTask[i].id = i + 1;
+  }
+  editTask(i) {
+    this.editStatus = true;
+    this.editName = this.arrTask[i].name;
+    this.taskID = i;
+  }
+  saveEdit() {
+    this.editStatus = false;
+    this.arrTask[this.taskID].name = this.editName;
+  }
+}
